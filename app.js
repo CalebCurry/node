3dc81859886a9e14c388a18a934df82fcc5fc537
Caleb Curry <calebcurry@Calebs-MacBook-Pro.local>
2020-02-10 08:41:37 -0500

    variable port

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ app.get('/', (req, res) => {
     res.send("Welcome to the home page baby");
 });
 
-app.listen(3000, () => {
+const port = process.env.port || 3000;
+app.listen(port, () => {
     console.log("Wazzappppp");
 });

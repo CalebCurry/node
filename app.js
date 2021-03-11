451017779c7225b54d1c114515bfcacf27a905e1
Lindany <mabasolindaniricardo@gmail.com>
2021-03-11 10:16:56 +0200

    change port

--- app.js
+++ app.js
@@ -1,11 +1,48 @@
-const express = require('express');
-const app = express();
+// index.js
 
-app.get('/', (req, res) => {
-    res.send("Updated");
-});
+/**
+ * Required External Modules
+ */
+ const express = require("express");
+ const path = require("path");
+ var compression = require('compression');
+ var helmet = require('helmet');
 
-const port = process.env.port || 3000;
-app.listen(port, () => {
-    console.log("Wazzappppp");
-});
+/**
+ * App Variables
+ */
+
+ const app = express();
+ const port = process.env.PORT || "8000";
+/**
+ *  App Configuration
+ */
+ app.use(compression()); //Compress all routes
+
+ app.set("views", path.join(__dirname, "views"));
+ app.set("view engine", "pug");
+ app.use(express.static(path.join(__dirname, "public")));
+
+/**
+ * Routes Definitions
+ */
+
+ app.get("/", (req, res) => {
+    res.render("index", { title: "Home" });
+  });
+
+  app.get("/user", (req, res) => {
+    res.render("user", { title: "Profile", userProfile: { nickname: "Auth0" } });
+  });
+
+  app.get("/logout", (req, res) => {
+    res.render("logout", { title: "Logout" });
+  });
+
+/**
+ * Server Activation
+ */
+
+ app.listen(port, () => {
+    console.log(`Listening to requests on http://localhost:${port}`);
+  });
